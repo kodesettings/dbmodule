057d8490cb38786f2af1dc8460ce7ee442600e6e
Marcell Nemes <marcellnemes98@gmail.com>
2025-05-07 13:57:20 +0200

    Added router apikey and user files for implementation

---
+++ internal/routes/user/router.go
@@ -0,0 +1,8 @@
+package routes_user
+
+import (
+	/*controllers*/ _ "github.com/kodesettings/dbmodule/v2/internal/controllers"
+	/*helpers*/ _ "github.com/kodesettings/dbmodule/v2/internal/helpers"
+	/*api_key*/ _ "github.com/kodesettings/dbmodule/v2/internal/auth/api_key"
+	/*middlewares*/ _ "github.com/kodesettings/dbmodule/v2/internal/auth/middlewares"
+)
