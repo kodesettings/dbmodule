b0dccfe833c5ff4400b2f6c1b24d8fbd47f40d75
Marcell Nemes <marcellnemes98@gmail.com>
2025-05-26 13:49:02 +0200

    Added context helper that was missing from previous commits

---
+++ sources/helpers/context.hpp
@@ -0,0 +1,11 @@
+#ifndef HELPERS_CONTEXT_H
+#define HELPERS_CONTEXT_H
+
+#include <string>
+#include <chrono>
+#include <array>
+
+typedef unsigned long uint64;
+typedef unsigned short uint16;
+
+#endif // HELPERS_CONTEXT_H
