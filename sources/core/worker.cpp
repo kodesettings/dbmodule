7f31f4c8eead4d32b1696a1051bdd904cc95151e
Marcell Nemes <marcellnemes98@gmail.com>
2025-05-28 21:48:49 +0200

    Updated project entry point implementation file

--- sources/core/worker.cpp
+++ sources/core/worker.cpp
@@ -4,22 +4,7 @@
 #endif
 #include "api_error.hpp"
 #include "api_response.hpp"
-
-// NOTE: this is for debugging only
-#include "../database/repository/api_key.cpp"
-#include "../database/repository/refresh_token.cpp"
-#include "../database/repository/user.cpp"
-#include "../controllers/api_key.cpp"
-#include "../controllers/user.cpp"
-
-#include "../helpers/context.hpp"
-#include "../helpers/mimecontent.hpp"
-#include "../helpers/utils.hpp"
-#include "../helpers/validator.hpp"
-
-#include "../auth/api_key/api_key.cpp"
-#include "../auth/middlewares/authentication.cpp"
-#include "../auth/middlewares/authorization.cpp"
-// ---
+#include "../auth/api_key/api_key.hpp"
+#include "../auth/middlewares/middlewares.hpp"
 
 // TODO: add javascript binding functions here
