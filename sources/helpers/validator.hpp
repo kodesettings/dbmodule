c71dcc3458b8174483cc49fb8d24a6f5422779de
Marcell Nemes <marcellnemes98@gmail.com>
2025-06-16 14:19:16 +0300

    Implemented valijson schema check in helpers validator

--- sources/helpers/validator.hpp
+++ sources/helpers/validator.hpp
@@ -3,6 +3,11 @@
 
 #include "../core/api_error.hpp"
 
+#include "valijson/adapters/picojson_adapter.hpp"
+#include "valijson/validation_results.hpp"
+#include "valijson/schema_parser.hpp"
+#include "valijson/validator.hpp"
+
 #define BODY "body"
 #define HEADER "headers"
 #define QUERY "query"
@@ -19,9 +24,33 @@ bool isAuthBearer(const std::string &value) {
   return true;
 }
 
-bool isJsonSchemaValid(const std::string &schema, const std::string &source, const std::string &req) {
-  // TODO: validate schema with valijson project
-  return true;
+#include "picojson/picojson.h"
+
+picojson::value jsonParse(std::string serialized, picojson::value def) {
+  picojson::value v;
+  std::string err;
+  picojson::parse(v, serialized.data(), serialized.data() + serialized.size(), &err);
+
+  if (!err.empty()) {
+    return def;
+  }
+
+  return v;
+}
+
+bool isJsonSchemaValid(const std::string &schemaStr, const std::string &source, const std::string &req) {
+  auto validatorSchema = std::make_shared<valijson::Schema>();
+  auto schemaJson = jsonParse(schemaStr, picojson::value{});
+  auto schemaAdapter = valijson::adapters::PicoJsonAdapter(schemaJson);
+  valijson::SchemaParser parser;
+  parser.populateSchema(schemaAdapter, *validatorSchema);
+
+  auto targetJson = jsonParse(req, picojson::value{});
+  auto targetAdapter = valijson::adapters::PicoJsonAdapter(targetJson);
+
+  valijson::ValidationResults results;
+  auto validator = valijson::Validator();
+  return validator.validate(*validatorSchema, targetAdapter, &results);
 }
 
 #endif // HELPERS_VALIDATOR_HPP
